7f89220e2cbf1c26dcc5f6584e6b3a315b881082
Ryan Manly <ryan.manly@gmail.com>
2015-09-22 12:12:09 -0500

    code deduplication

--- chrome_quic_disabler.py
+++ chrome_quic_disabler.py
@@ -6,6 +6,14 @@ import os
 path = '~/Library/Application Support/Google/Chrome/Local State'
 local_state = os.path.expanduser(path)
 
+
+def write_json(contents, file=local_state):
+    json_contents = json.JSONEncoder().encode(contents)
+
+    with open(local_state, 'w') as f:
+        f.write(str(json_contents))
+
+
 if os.path.exists(local_state):
     with open(local_state) as f:
         contents = json.load(f)
@@ -13,16 +21,9 @@ if os.path.exists(local_state):
     # enabled == 1, disabled == 2, default == unlisted in local state file
     contents['browser']['enabled_labs_experiments'] = [u'enable-quic@2']
 
-    # I got an error the first time I wrote this out because
-    # I forgot to turn it back into proper JSON
-    json_contents = json.JSONEncoder().encode(contents)
-
-    with open(local_state, 'w') as f:
-        f.write(str(json_contents))
+    write_json(contents)
 else:
     new_contents = {}
     new_contents[u'browser'] = {u'enabled_labs_experiments': [u'enable-quic@2']}
-    json_contents = json.JSONEncoder().encode(new_contents)
 
-    with open(local_state, 'w') as f:
-       f.write(json_contents) 
+    write_json(new_contents)
